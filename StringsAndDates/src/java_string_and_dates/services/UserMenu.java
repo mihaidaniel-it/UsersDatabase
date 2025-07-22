c8cfe65e3e630ca5d705072067db44c05c5e96c7
Daniel <mihaidaniel.it@gmail.com>
2025-07-22 11:28:55 +0300

    Add instruction for user

--- StringsAndDates/src/java_string_and_dates/services/UserMenu.java
+++ StringsAndDates/src/java_string_and_dates/services/UserMenu.java
@@ -1,10 +1,10 @@
 package java_string_and_dates.services;
 
 import java.util.Scanner;
-import java_string_and_dates.services.UserOption;
 
 public class UserMenu {
     static UserOption userOption;
+    private static boolean hasPrinted = false;
 
 //    Методы
     public static void selectMenuOption() {
@@ -12,8 +12,15 @@ public class UserMenu {
         FinancialAccounting financialAccounting = new FinancialAccounting();
         Scanner scanner = new Scanner(System.in);
 
+//        Вывод первичной строки о помощи при запуске
+        if (hasPrinted) {
+            System.out.println(" ");
+        } else if (!hasPrinted) {
+            System.out.println("Команда 'HELP' - выведет инструкции по пользованию");
+            hasPrinted = true;
+        }
+
 //        Получение данных
-        System.out.println("Команда 'HELP' - выведет инструкции по пользованию");
         System.out.print("Введите команду для выполнения операций: ");
         String selectedOption = scanner.nextLine();
 
@@ -23,10 +30,21 @@ public class UserMenu {
             financialAccounting.addTransaction();
         } else {
             switch (verifyOption) {
-                case HELP -> System.out.println("Была введена команда HELP");
+                case HELP -> helpOptionMenu();
                 case REPORT -> System.out.println("Была введена команда REPORT");
                 case EXIT -> System.out.println("Была введена команда EXIT");
             }
         }
+    }
+
+    public static void helpOptionMenu() {
+        System.out.println(" ");
+        System.out.println("Инструкция по использованию приложения:");
+        System.out.println("Команда  'HELP' - позволяет получить помощь по приложению");
+        System.out.println("Команда  'REPORT' - покажет ваш отчет по финансам");
+        System.out.println("Команда  'EXIT' - закроет приложение и покажет ваш отчет по финансам");
+        System.out.println("Если вы хотите добавить новую транзакцию, в поле ввода команды нажмите Enter");
+
+        selectMenuOption();
     }
 }

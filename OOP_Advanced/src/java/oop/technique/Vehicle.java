acaa2c29cffc414a08de1779b77b64add5987209
Daniel <mihaidaniel.it@gmail.com>
2025-07-24 13:33:13 +0300

    Implenets interface classes & add methods

--- OOP_Advanced/src/java/oop/technique/Vehicle.java
+++ OOP_Advanced/src/java/oop/technique/Vehicle.java
@@ -1,4 +1,61 @@
 package java.oop.technique;
 
-public class Vehicle {
+import java.oop.explotation.Acceleration;
+import java.oop.explotation.Breakable;
+import java.oop.explotation.Cargo;
+import java.oop.explotation.Refuelled;
+
+public abstract class Vehicle implements Acceleration, Breakable, Cargo, Refuelled {
+//    Поля
+    private String model;
+    private double vehicleCondition = Math.random() * 100;
+    private double vehicleWeight;
+    private int averageSpeed;
+    private int tankVolume;
+    private int x = 0;
+
+//    Конструктор
+    public Vehicle(String model, double vehicleWeight, int averageSpeed, int tankVolume) {
+        this.model = model;
+        this.vehicleWeight = vehicleWeight;
+        this.averageSpeed = averageSpeed;
+        this.tankVolume = tankVolume;
+    }
+
+//    Методы
+    public void move() {
+        this.x += averageSpeed;
+    }
+
+    @Override
+    public void acceleration() {
+        averageSpeed += 5;
+    }
+
+    @Override
+    public boolean isBroken() {
+        if (vehicleCondition < 45) {
+            return true;
+        } else {
+            System.out.println("Ваша машина сломалась. Гонка завершена!");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean isCargo() {
+        if (vehicleWeight < 2500) {
+            averageSpeed += 5;
+            return false;
+        } else {
+            averageSpeed -= 5;
+            return true;
+        }
+    }
+
+    public void refuel() {
+        if (tankVolume > 45) {
+            acceleration();
+        }
+    }
 }

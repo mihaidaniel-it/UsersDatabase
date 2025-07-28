a5408f1f0b19b98cd03e0f44496b67b16ac9fdad
Daniel <mihaidaniel.it@gmail.com>
2025-07-28 14:44:01 +0300

    Add methods which show menu and perform functions

--- Collections_advanced/src/java_collections_advanced/CollectionsAdvanced.java
+++ Collections_advanced/src/java_collections_advanced/CollectionsAdvanced.java
@@ -1,7 +1,10 @@
 package java_collections_advanced;
 
+import java_collections_advanced.services.MenuNavigator;
+
 public class CollectionsAdvanced {
     public static void main(String[] args) {
-
+        MenuNavigator menuNavigator = new MenuNavigator();
+        menuNavigator.showMenu();
     }
 }
--- Collections_advanced/src/java_collections_advanced/services/MenuNavigator.java
+++ Collections_advanced/src/java_collections_advanced/services/MenuNavigator.java
@@ -1,4 +1,32 @@
 package java_collections_advanced.services;
 
+import java.util.Scanner;
+
 public class MenuNavigator {
+//    Методы
+    public void showMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите команду: ");
+        String selectedOption = scanner.nextLine();
+        checkOption(selectedOption);
+    }
+
+    public void checkOption(String option) {
+        String optionToLowerCase = option.toLowerCase();
+
+        switch (optionToLowerCase) {
+            case "in" -> System.out.println("Выбрана опция " + optionToLowerCase);
+            case "del" -> System.out.println("Выбрана опция " + option);
+            case "count" -> System.out.println("Выбрана опция " + option);
+            case "avg" -> System.out.println("Выбрана опция " + option);
+            case "median" -> System.out.println("Выбрана опция " + option);
+            case "young" -> System.out.println("Выбрана опция " + option);
+            case "old" -> System.out.println("Выбрана опция " + option);
+            case "print" -> System.out.println("Выбрана опция " + option);
+            case "help" -> System.out.println("Выбрана опция " + option);
+            case "exit" -> System.out.println("Выбрана опция " + option);
+            default -> System.out.println("Неизвестная команда. Попробуйте снова");
+        }
+    }
 }

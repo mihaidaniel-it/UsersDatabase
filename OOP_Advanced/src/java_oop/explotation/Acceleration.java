3f7d2a370f6416122bc46c62f841aa3fc5e2d858
Daniel Mihai <mihaidaniel.it@gmail.com>
2025-07-29 12:39:13 +0300

    Delete OOP_Advanced directory

--- OOP_Advanced/src/java_oop/explotation/Acceleration.java
+++
@@ -1,5 +0,0 @@
-package java_oop.explotation;
-
-public interface Acceleration {
-    boolean acceleration();
-}
--- OOP_Advanced/src/java_oop/explotation/Viability.java
+++
@@ -1,5 +0,0 @@
-package java_oop.explotation;
-
-public interface Viability {
-    int transportViability();
-}

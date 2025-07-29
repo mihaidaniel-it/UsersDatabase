f360833a9fe9827e92886a9db8facfa1e9e8a301
Daniel Mihai <mihaidaniel.it@gmail.com>
2025-07-29 12:39:25 +0300

    Delete StringsAndDates directory

--- StringsAndDates/src/java_string_and_dates/Main.java
+++
@@ -1,8 +0,0 @@
-package java_string_and_dates;
-import java_string_and_dates.services.UserMenu;
-
-public class Main {
-    public static void main(String[] args) {
-        UserMenu.selectMenuOption();
-    }
-}
--- StringsAndDates/src/java_string_and_dates/model/Transaction.java
+++
@@ -1,9 +0,0 @@
-package java_string_and_dates.model;
-
-public record Transaction(
-        String transactionDescription,
-        double transactionValue,
-        TransactionType transactionType,
-        String transactionDate
-) {
-}
